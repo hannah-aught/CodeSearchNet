bc9e5c2d49fcb6db12a121e2cc3e3f90682672ca
Hannah Brown <hsbrown@ucdavis.edu>
2020-12-06 23:24:36 -0800

    added ability to compare data in multiple batches instead of only as one dataframe (for memory reasons)

--- src/dataextraction/dedup_split.py
+++ src/dataextraction/dedup_split.py
@@ -38,40 +38,93 @@ from dpu_utils.utils import RichPath, run_and_debug
 from dpu_utils.codeutils.deduplication import DuplicateDetector
 import os
 from tqdm import tqdm
+import random
+import math
+import json
 
+def get_files_remaining(input_path):
+    all_files = list(input_path.iterate_filtered_files_in_dir('*.jsonl.gz'))
+    remaining_files = {str(f):set() for f in all_files}
+    return remaining_files
 
-def jsonl_to_df(input_folder: RichPath) -> pd.DataFrame:
+def df_to_jsonl(df, project_map, input_folder):
+    for path in project_map:
+        with open(path, 'w') as f:
+            split_df = pd.DataFrame()
+            for project in project_map[path]:
+                split_df = pd.concat([split_df, df[df.nwo == project]])
+            input_folder.join(path.split('/')[-1]).save_as_compressed_file(split_df.to_dict(orient='records'))
+
+def jsonl_to_df(input_folder: RichPath, sample_percent: float, files_remaining: dict, azure_info_path) -> pd.DataFrame:
     "Concatenates all jsonl files from path and returns them as a single pandas.DataFrame ."
 
     assert input_folder.is_dir(), 'Argument supplied must be a directory'
     dfs = []
-    files = list(input_folder.iterate_filtered_files_in_dir('*.jsonl.gz'))
+    all_files = list(input_folder.iterate_filtered_files_in_dir('*.jsonl.gz'))
+    
+    sample_size = math.ceil(sample_percent * len(all_files))
+
+    if sample_size > len(files_remaining):
+        sample_size = len(files_remaining)
+    files = random.sample(files_remaining.keys(), sample_size)
+    replaced = [0 for x in range(sample_size)]
+
+    while True:
+        for i in range(len(files)):
+            f = files[i]
+            other_files = {x for x in files if x != f}
+            if f not in files_remaining or len(set.intersection(files_remaining[f], other_files)) == len(files) - 1:
+                replaced[i] = 1
+                f = random.sample(files_remaining.keys(), 1)
+                while f[0] in files:
+                    f = random.sample(files_remaining.keys(), 1)
+                files[i] = f[0]
+            else:
+                replaced[i] = 0
+        if sum(replaced) < 2:
+            break
+
+    for f in files:
+        files_remaining[f] = files_remaining[f].union({x for x in files if x != f})
+        if len(files_remaining[f]) == len(all_files) - 1:
+            del files_remaining[f]
+        with open('files_remaining.txt', 'w+') as f:
+            files_remaining_converted = {}
+            
+            for path in files_remaining:
+                files_remaining_converted[path] = list(files_remaining[path])
+
+            print(json.dumps(files_remaining_converted), file = f)
+
     assert files, 'There were no jsonl.gz files in the specified directory.'
     print(f'reading files from {input_folder.path}')
+    project_map = {x:[] for x in files}
+    print(project_map)
     for f in tqdm(files, total=len(files)):
-        lines = list(f.read_as_jsonl(error_handling=lambda m,e: print(f'Error while loading {m} : {e}')))
+        rich_f = RichPath.create(f, azure_info_path)
+        lines = list(rich_f.read_as_jsonl(error_handling=lambda m,e: print(f'Error while loading {m} : {e}')))
         lines_with_docstrings = []
 
         for line in lines:
             if len(line['docstring_tokens']) > 0:
-                line['func_name'] = line.pop('identifier')
-                line['code'] = line.pop('function')
-                line['code_tokens'] = line.pop('function_tokens')
                 lines_with_docstrings.append(line)
+                
+                if line['nwo'] not in project_map[str(rich_f)]:
+                    project_map[str(rich_f)].append(line['nwo'])
         
         dfs.append(pd.DataFrame(lines_with_docstrings))
-    return pd.concat(dfs)
+    return pd.concat(dfs), project_map
 
 
 def remove_duplicate_code_df(df: pd.DataFrame) -> pd.DataFrame:
     "Resolve near duplicates based upon function_tokens field in data."
-    assert 'code_tokens' in df.columns.values, 'Data must contain field code_tokens'
+    assert 'function_tokens' in df.columns.values, 'Data must contain field function_tokens'
     assert 'language' in df.columns.values, 'Data must contain field language'
     df.reset_index(inplace=True, drop=True)
     df['doc_id'] = df.index.values
     dd = DuplicateDetector(min_num_tokens_per_document=10)
     filter_mask = df.apply(lambda x: dd.add_file(id=x.doc_id,
-                                                 tokens=x.code_tokens,
+                                                 tokens=x.function_tokens,
                                                  language=x.language),
                            axis=1)
     # compute fuzzy duplicates
@@ -117,6 +170,7 @@ def label_folds(df: pd.DataFrame, train_ratio: float, valid_ratio: float, test_r
 
 
 def run(args):
+    batch_size = .25
 
     azure_info_path = args.get('--azure-info', None)
     input_path = RichPath.create(args['INPUT_FILENAME'], azure_info_path)
@@ -127,9 +181,17 @@ def run(args):
     holdout = float(args['--holdout-ratio'])
 
     # get data and process it
-    df = jsonl_to_df(input_path)
-    #print('Removing fuzzy duplicates ... this may take some time.')
-    #df = remove_duplicate_code_df(df)
+    files_remaining = get_files_remaining(input_path)
+
+    while len(files_remaining) > 0:
+        print(files_remaining)
+        df, project_map = jsonl_to_df(input_path, batch_size, files_remaining, azure_info_path)
+
+        print('Removing fuzzy duplicates ... this may take some time.')
+        df = remove_duplicate_code_df(df)
+        df_to_jsonl(df, project_map, input_path)
+
+    
     df = df.sample(frac=1, random_state=20181026)  # shuffle order of files
     df = label_folds(df, train_ratio=train, valid_ratio=valid, test_ratio=test, holdout_ratio=holdout)
     splits = ['train', 'valid', 'test', 'holdout']
