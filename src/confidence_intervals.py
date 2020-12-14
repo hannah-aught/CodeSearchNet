eb264290f88b2639c052baffb0be484becc35b80
Hannah Brown <hsbrown@ucdavis.edu>
2020-12-14 10:29:00 -0800

    added multiprocessing to speed up evaluation

--- src/confidence_intervals.py
+++ src/confidence_intervals.py
@@ -15,6 +15,7 @@ Options:
     --dryrun                         Do not log run into logging database. [default: False]
     --azure-info PATH                Azure authentication information file (JSON). Used to load data from Azure storage.
     --sequential                     Do not parallelise data-loading. Simplifies debugging. [default: False]
+    --processes NUMBER               Number of processes to use during testing. [default: 16]
     --alpha VALUE                    Value to use for confidence level in the confidence interval. [default: 0.95]
     --debug                          Enable debug routines. [default: False]
 """
@@ -25,6 +26,8 @@ from dpu_utils.utils import run_and_debug, RichPath
 import model_test as test
 import scipy.stats as st
 import numpy as np
+from multiprocessing import Pool
+import functools
 
 def get_confidence_interval(data, alpha):
     return st.t.interval(alpha=alpha, df=len(data)-1, loc=np.mean(data), scale=st.sem(data))
@@ -35,24 +38,18 @@ def run(arguments):
     test_data_dir = test.expand_data_path(arguments['TEST_DATA_PATH'], azure_info_path)
     model_paths = RichPath.create(arguments['MODEL_PATH'], azure_info_path=azure_info_path).get_filtered_files_in_dir('*.pkl.gz')
     alpha = float(args['--alpha'])
-    results = []
 
-    for path in model_paths:
-        results.append(test.compute_evaluation_metrics(path, arguments, azure_info_path, 
-                                                       valid_data_dir, test_data_dir, return_results=True)['All'])
-    
-    mrrs = list(zip(*results))
-    docstring_mrrs = mrrs[0]
-    func_name_mrrs = mrrs[1]
-    validation_mrrs = mrrs[2]
+    with Pool(int(arguments['--processes'])) as pool:
+        results = pool.map(functools.partial(test.compute_evaluation_metrics, arguments=arguments, azure_info_path=azure_info_path, valid_data_dirs=valid_data_dir, test_data_dirs=test_data_dir, return_results=True, languages=['java'], test_valid=False), model_paths)
+
+    docstring_mrrs = [x['java'][0] for x in results]
+    func_name_mrrs = [x['java'][1] for x in results]
 
     docstring_confidence = get_confidence_interval(docstring_mrrs, alpha)
     func_name_confidence = get_confidence_interval(func_name_mrrs, alpha)
-    validation_confidence = get_confidence_interval(validation_mrrs, alpha)
-    
+
     print(f'{alpha*100}% confidence interval for mrr using docstring as the query: {docstring_confidence}')
     print(f'{alpha*100}% confidence interval for mrr using function name as the query: {func_name_confidence}')
-    print(f'{alpha*100}% confidence interval for mrr using docstring as the query on validation data: {validation_confidence}')
 
 if __name__ == '__main__':
     args = docopt(__doc__)
